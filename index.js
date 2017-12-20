c19aaac79830f6288d59896815642820bd78f5ad
Sebastián Salata R-T <sa.salatart@gmail.com>
2017-12-19 22:44:26 -0300

    ✨ add failed build listener

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Botkit = require('botkit');
-const { searchGifFor } = require('./src/services/giphy-service');
+const { failedBuildListener } = require('./src/listeners/failed-build');
 
 const controller = Botkit.slackbot({
   debug: process.env.NODE_ENV === 'production',
@@ -9,7 +9,4 @@ controller.spawn({
   token: process.env.SLACK_TOKEN,
 }).startRTM();
 
-controller.hears('build failed', 'ambient', async (bot, message) => {
-  const gifUrl = await searchGifFor('shame');
-  bot.reply(message, gifUrl);
-});
+controller.on('bot_message', failedBuildListener);
