51ff3ee574518f9c01d4152606cd6ebcb96c5d58
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-03-15 19:52:09 -0300

    ✨ show shamers with same amount of shames in the same position in ranking

--- src/listeners/shamers.js
+++ src/listeners/shamers.js
@@ -6,13 +6,16 @@ const { dialogues: { replies } } = require('../utils/conversations');
 const { robohash } = require('../utils/robohash');
 const config = require('../config');
 
-function generateRanking(shames) {
-  return Object.keys(shames)
-    .sort((shamerA, shamerB) => shames[shamerB] - shames[shamerA])
-    .map((shamerName, index) => {
-      const shamesNumber = `${shames[shamerName]} shame${shames[shamerName] > 1 ? 's' : ''}`;
+function generateRankingFields(rawShames) {
+  const shamersByShames = _.groupBy(Object.keys(rawShames), userName => rawShames[userName]);
+
+  return Object.keys(shamersByShames)
+    .sort((shamesA, shamesB) => shamesB - shamesA)
+    .map((shames, index) => {
+      const shamesNumber = `${shames} shame${shames > 1 ? 's' : ''}`;
+      const shamers = shamersByShames[shames].join(', ');
       return {
-        title: `#${index + 1} ${shamerName} (${shamesNumber})`,
+        title: `#${index + 1} ${shamers} (${shamesNumber})`,
         short: false,
       };
     });
@@ -28,11 +31,11 @@ function showRankingListener(controller) {
 
     const reply = snakeize({
       username: config.botName,
-      iconUrl: robohash('Ranking'),
+      iconUrl: robohash('ranking'),
       attachments: [{
         pretext: replies.ranking.shamersSoFar,
         color: config.attachments.color,
-        fields: generateRanking(shames),
+        fields: generateRankingFields(shames),
       }],
     });
 
